e4f6c673b8e474806b812daeb44dcd33cc9f4cdd
TheBestInge <drodriguezrod@unal.edu.co>
2024-03-05 10:30:10 -0500

    Mas cositas

---
+++ Practica codeforces/Wrong Subtraction.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+//{}  []
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n,k; cin>>n>>k;
+    while(k--){
+        n%10==0 ? n=n/10 : n--;
+    }
+    cout<<n<<endl;
+
+    return 0;
+}
